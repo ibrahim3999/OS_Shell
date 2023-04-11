139649b85b6f26693b0ef4538f9afbac53c417a5
ibrahim3999 <mgdlhb@gmail.com>
2023-04-12 00:23:36 +0300

    PART A copy command done

--- basic_shell.c
+++ basic_shell.c
@@ -46,7 +46,6 @@ int cmp_files(const char *file1, const char *file2, const char *flag) {
             break;
         }
     }
-
     if (equal==1 && !strcmp(flag,"-v")) {
         printf("equal\n");
     } else if  (!strcmp(flag,"-v")){
@@ -58,15 +57,82 @@ int cmp_files(const char *file1, const char *file2, const char *flag) {
     return equal ? 0 : 1;
 }
 
+
+int copy_file(const char* source_file, const char* dest_file, int force_flag, int verbose_flag) {
+    FILE* source_fp;
+    FILE* dest_fp;
+    char buffer[1024];
+    int bytes_read;
+    int success = 0;
+
+    if (access(dest_file, F_OK) != -1 && verbose_flag) {
+      printf("target file exist\n");
+      return 0;
+   }
+
+    source_fp = fopen(source_file, "rb");
+    if (source_fp == NULL) {
+        fprintf(stderr, "Error opening source file '%s': %s\n", source_file, strerror(errno));
+        return -1;
+    }
+
+    dest_fp = fopen(dest_file, force_flag ? "wb" : "ab");
+    if (dest_fp == NULL) {
+        fprintf(stderr, "Error opening destination file '%s': %s\n", dest_file, strerror(errno));
+        fclose(source_fp);
+        return -1;
+    }
+
+    if (!force_flag && ftell(dest_fp) > 0) {
+        fprintf(stderr, "Target file '%s' already exists. Use -f flag to overwrite.\n", dest_file);
+        fclose(source_fp);
+        fclose(dest_fp);
+        return 1;
+    }
+
+    while ((bytes_read = fread(buffer, 1, sizeof(buffer), source_fp)) > 0) {
+        if (fwrite(buffer, 1, bytes_read, dest_fp) != bytes_read) {
+            fprintf(stderr, "Error writing to destination file '%s': %s\n", dest_file, strerror(errno));
+            success = 1;
+            break;
+        }
+    }
+
+    if (ferror(source_fp)) {
+        fprintf(stderr, "Error reading from source file '%s': %s\n", source_file, strerror(errno));
+        success = 1;
+    }
+
+    if (fclose(source_fp) != 0) {
+        fprintf(stderr, "Error closing source file '%s': %s\n", source_file, strerror(errno));
+        success = 1;
+    }
+
+    if (fclose(dest_fp) != 0) {
+        fprintf(stderr, "Error closing destination file '%s': %s\n", dest_file, strerror(errno));
+        success = 1;
+    }
+
+    if (success == 0 && verbose_flag) {
+        printf("success\n");
+    }else if (success && verbose_flag)
+    {
+        printf("general failure\n");
+    }
+    
+    return success;
+}
+
 int main() {
 	int i;
 	char *argv[10];
 	char command[1024];
 	char *token;
-	char *file1;
-	char *file2;
+    char *comm0;
+	char *comm1;
+	char *comm2;
 	char *flag;
-	
+
 
 	while (1) {
 	    printf("hello: ");
@@ -75,34 +141,50 @@ int main() {
 
 		//int res=cmp("Files/a.txt","Files/b.txt");
 		//printf("%d\n",res);
-		   
+   
 	 /* parse command line */
 		
 	    i = 0;
-	    token = strtok (command," ");
+	    token = strtok (command," ");//split when space
 		
 	    while (token != NULL){
-		argv[i] = token;
-		token = strtok (NULL, " ");
+            argv[i] = token;
+            if(i==0){
+                comm0=token;
+            }
+            token = strtok (NULL," ");
 
-		if(i==0){
-			file1=token;
-		}
-		if(i==1){
-			file2=token;
-		}
-		if(i==2){
-			flag=token;
-		}
-		i++;
+            if(i==0){
+                comm1=token;
+            }
+            if(i==1){
+                comm2=token;
+            }
+            if(i==2){
+                flag=token;
+            }
+            i++;
 	    }
-		if(flag!=NULL){
-			cmp_files(file1,file2,flag);
-		}
-		else{
-			cmp_files(file1,file2,"");
-		}
-		
+       
+        if(!strcmp(comm0,"cmp")){
+            if(flag!=NULL){
+                cmp_files(comm1,comm2,flag);
+            }
+            else{
+                cmp_files(comm1,comm2,"");
+            }
+        }
+		else if(!strcmp(comm0,"copy")){
+            if(!strcmp(flag,"-v")){
+                copy_file(comm1,comm2,0,1);
+            }
+            if(!strcmp(flag,"-f")){
+                copy_file(comm1,comm2,1,1);
+            }
+            
+        }
+
+
 		//printf("%d\n",res_cmp);
 		
 
@@ -120,4 +202,5 @@ int main() {
 	    }
 		*/    
 	}
+    
 }
