dda49e8a20638f3a3d19e80bb38e8f6bfa6101ed
ibrahim3999 <mgdlhb@gmail.com>
2023-04-20 18:42:18 +0300

    Done project

--- basic_shell.c
+++ basic_shell.c
@@ -13,22 +13,55 @@
 #include "source/codecB.h"
 #include <signal.h> //for signal handling
 #include <sys/wait.h>
+#define MAX_ARGS 10
 
 
 //signal handler function for SIGINT
-void handle_signal(int sig) {
+void handle_signal() {
     printf("\nCtrl+C detected, quitting the shell.\n");
     exit(EXIT_SUCCESS);
 }
+int execute_command(char *argv[], int input_fd, int output_fd) {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        // Child process
+
+        if (input_fd != STDIN_FILENO) {
+            dup2(input_fd, STDIN_FILENO);
+            close(input_fd);
+        }
+        if (output_fd != STDOUT_FILENO) {
+            dup2(output_fd, STDOUT_FILENO);
+            close(output_fd);
+        }
+
+        execvp(argv[0], argv);
+        perror("exec failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0) {
+        // Fork failed
+        perror("Fork failed");
+        return -1;
+    }
+    else {
+        // Parent process
+        wait(NULL);
+        return 0;
+    }
+}
 
 int main() {
-	char *argv[10];
+	char *argv[MAX_ARGS];
 	char command[1024];
 	char *token;
     char *saveptr;
     int argCount = 0;
     char *output = NULL;
     int append = 0;
+    int input_fd = STDIN_FILENO;
+    int output_fd = STDOUT_FILENO;
 
     signal(SIGINT,handle_signal);// register SIGINT singl hanlder
 
@@ -36,36 +69,54 @@ int main() {
         argCount = 0;
         output = NULL;
         append = 0;
-	    printf("hello: ");
+        input_fd = STDIN_FILENO;
+        output_fd = STDOUT_FILENO;
+	    printf("$:");
 	    fgets(command, 1024, stdin);
 	    command[strlen(command) - 1] = '\0'; // replace \n with \0
       
-   
-	 /* parse command line */
 		
 	    token = strtok_r(command," ",&saveptr);//split when space
 		
-	 while (token != NULL) {
+        while (token != NULL) {
 
-        if (strcmp(token, ">>") == 0) {
-            token = strtok_r(NULL, " ", &saveptr);
-            if (token != NULL) {
-                output = token;
-                append = 1;
+            if (strcmp(token, ">>") == 0) {
+                token = strtok_r(NULL, " ", &saveptr);
+                if (token != NULL) {
+                    output = token;
+                    append = 1;
+                }
+            } else if (strcmp(token, ">") == 0) {
+                token = strtok_r(NULL, " ", &saveptr);
+                if (token != NULL) {
+                    output = token;
+                    append = 0;
+                }
+            }else if (strcmp(token, "|") == 0) {
+                // Execute left-hand side of the pipe
+                argv[argCount] = NULL;
+                int pipefd[2];
+                pipe(pipefd);
+                execute_command(argv, input_fd, pipefd[1]);
+                close(pipefd[1]);
+                input_fd = pipefd[0];
+                argCount = 0;
             }
-        } else if (strcmp(token, ">") == 0) {
-            token = strtok_r(NULL, " ", &saveptr);
-            if (token != NULL) {
-                output = token;
-                append = 0;
+            else {
+                argv[argCount++] = token;
             }
-        } else {
-            argv[argCount++] = token;
+            token = strtok_r(NULL, " ", &saveptr);
         }
-        token = strtok_r(NULL, " ", &saveptr);
-     }
         argv[argCount]=NULL;
-
+         /* set output file descriptor */
+        if (output != NULL) {
+            if (append) {
+                output_fd = open(output, O_CREAT | O_WRONLY | O_APPEND, S_IRWXU);
+            } else {
+                output_fd = open(output, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+            }
+        }
+        /* handle built-in commands */
         if(!strcmp(argv[0],"cmp")){
             if(argv[3]!=NULL){
                 cmp_files(argv[1],argv[2],argv[3]);
@@ -130,38 +181,10 @@ int main() {
             continue;
         } 
       
-	    /* for commands not part of the shell command language */ 
-		pid_t pid = fork();
-
-        if (pid == 0) { 
-            // Child process
-
-            if (output != NULL) {
-                int flags = O_CREAT | O_WRONLY;
-                if (append) {
-                    flags |= O_APPEND;
-                } else {
-                    flags |= O_TRUNC;
-                }
-                int fd = open(output, flags, 0644);
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-            }
-            execvp(argv[0], argv);
-            perror("exec failed");
-            exit(EXIT_FAILURE);
-        }
-        else if (pid < 0) {
-            // Fork failed
-            perror("Fork failed");
-            exit(EXIT_FAILURE);
-        }
-        else {
-            // Parent process
-            wait(NULL);
-            // Cleanup
-        }
-		    
-	}
+	    /* execute command */
+        execute_command(argv, input_fd, output_fd);
+            
+    }
+    return 0;
    
 }
